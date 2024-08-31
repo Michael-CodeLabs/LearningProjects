9418b889851ea5e90248932ba594a56243fadeb5
Michael Cold <deutxse@gmail.com>
2024-08-31 20:05:23 +0200

    Patch Notes 0.1 Fixed: Fixed the bug where both doubles and triples would apply at the same time. Restructured Code with better logic.
    Added prizes depending on total roll.

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -5,26 +5,37 @@ int roll3 = dice.Next(0,7);
 
 int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if (total >= 15)
-{
-    Console.WriteLine("You Win!");
-}
-
-if (total < 15)
-{
-    Console.WriteLine("Better Luck Next Time...");
-}
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles!\n+2 Bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if ((roll1 == roll2 ) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled Triples!\n+6 Bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total >= 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }

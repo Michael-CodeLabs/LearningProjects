51b4aac58de3f201b11c6a1fe512a227c44843f9
Michael Cold <deutxse@gmail.com>
2024-08-31 23:42:06 +0200

    Learned about Arrays. for refrence Store and iterate through sequences of data using Arrays and the foreach statement in C

---
+++ arrays_securityDepartment/Program.cs
@@ -0,0 +1,19 @@
+﻿//string[] fradulentOrderIDs = new string[3];
+
+/*fradulentOrderIDs[0] = "A123";
+fradulentOrderIDs[1] = "B456";
+fradulentOrderIDs[2] = "C789";
+//fradulentOrderIDs[3] = "D000";
+*/
+
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");

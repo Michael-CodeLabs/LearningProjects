8e0b1ee9ed3aa8802245730c4ca9e691635cdf8c
Michael Cold <deutxse@gmail.com>
2024-08-31 19:48:34 +0200

    main

---
+++ DiceGame/Program.cs
@@ -0,0 +1,30 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next(0,7);
+int roll2 = dice.Next(0,7);
+int roll3 = dice.Next(0,7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total >= 15)
+{
+    Console.WriteLine("You Win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Better Luck Next Time...");
+}
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles!\n+2 Bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2 ) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled Triples!\n+6 Bonus to total!");
+    total += 6;
+}
